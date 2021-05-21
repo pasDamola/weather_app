35571a22d7c8e0acc6ff2768cdc2734f862157b8
Oyinda David <oyindadavid18@gmail.com>
2021-05-21 18:44:43 +0100

    Learning and implementing the callback pattern

---
+++ utils/geocode.js
@@ -0,0 +1,42 @@
+const request = require('request')
+
+const url = 'http://api.weatherstack.com/current?access_key=28ad3f4c227aee60cd4eed5feaca47da&query=New%20York'
+
+
+request({ url: url, json: true}, (err, res) => {
+    const {temperature, feelslike} = res.body.current
+    console.log(`It is currently ${temperature} degrees out. It feels like ${feelslike}`)
+})
+
+
+const geoCode = (address, callback) => {
+    const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1Ijoib3lpbmNvZGUiLCJhIjoiY2tveWI5bGQ1MDFiZjJvbjNhaHAxYmtwdSJ9.pI6hfDM1fIK6Ey-urDmn8g&limit=1`
+    request({ url: url, json: true}, (error, response) => {
+        if(error) {
+            callback('Unable to connect to location services', undefined)
+        } else if(response.body.features.length === 0) {
+            callback('Unable to find location, Try another search', undefined)
+        } else {
+            callback(undefined, {
+               longitude: response.body.features[0].center[0],
+               latitude: response.body.features[0].center[1],
+               location: response.body.features[0].place_name
+            })
+        }
+    })
+
+}
+
+
+geoCode('Abuja', (error, data) => {
+    if(error) {
+        console.log(error)
+    }
+    console.log('Data', data)
+})
+
+
+
+
+
+module.exports = geoCode
